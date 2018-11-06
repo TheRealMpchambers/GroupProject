86820616a2bcec1728e28f0b2b4f5c40a8cb65ec
Max <maxniederluecke@gmail.com>
2018-11-05 20:27:08 -0600

    created event page and updated API

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,38 +1,48 @@
 $(document).ready(function(){
 	console.log("ready");
 
-	var keyword = "";
 
-	var tmUrl = keyword;
+	$("#submitBtn").on("click", function(){
+		var keyword = $("#keywordIn").val().trim();
+		var startDate = $("#startDateIn").val().trim();
+		var endDate = $("#endDateIn").val().trim();
+		var city = $("#cityIn").val().trim();
+		var radius = $("#radiusIn").val().trim();
 
-	// $("#button").on("click", function(){
-		$.ajax({
-			method: "GET",
-		  	url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=s13JIGsAVU63A3yIEMWPlQSQt5ldZz7x",
-		  	async: true,
-		 	dataType: "json",
+		var tmUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=5&apikey=s13JIGsAVU63A3yIEMWPlQSQt5ldZz7x&format=json&keyword=" + keyword + "&radius=" + radius+ "&startDateTime=" + startDate + "&endDateTime= " + endDate + "&city=" + city;
 
-		}).then(function(results){
-			console.log(results);
-			$("tbody").empty();
-			for (i = 0; i < results._embedded.events.length; i++){
-				tRow = $("<tr>");
-				event = $("<td>");
-				event.text(results._embedded.events[i].name);
-				venue = $("<td>");
-				venue.text(results._embedded.events[i]._embedded.venues[0].name);
-				date = $("<td>");
-				date.text(results._embedded.events[i].dates.start.localDate)
-				ticketPrice = $("<td>");
-				ticketPrice.text("$" + results._embedded.events[i].priceRanges[0].min)
-				link = $("<td>");
-				tmLink = $("<img>");
-				tmLink.attr("src", results._embedded.events[i].url);
-				link.append(tmLink);
-				tRow.append(event, venue, date, ticketPrice, link);
-				$("tbody").append(tRow)
-			}
-		});
+			$.ajax({
+				method: "GET",
+			  	url: tmUrl,
+			  	async: true,
+			 	dataType: "json",
+
+			}).then(function(results){
+				console.log(results);
+				$("tbody").empty();
+				for (i = 0; i < results._embedded.events.length; i++){
+					tRow = $("<tr>");
+					event = $("<td>");
+					event.text(results._embedded.events[i].name);
+					venue = $("<td>");
+					venue.text(results._embedded.events[i]._embedded.venues[0].name);
+					date = $("<td>");
+					date.text(results._embedded.events[i].dates.start.localDate)
+					ticketPrice = $("<td>");
+					ticketPrice.text("$" + results._embedded.events[i].priceRanges[0].min)
+					link = $("<td>");
+					tmLink = $("<a>").text("link");
+					tmLink.attr("href", results._embedded.events[i].url);
+					link.append(tmLink);
+					tRow.append(event, venue, date, ticketPrice, link);
+					$("tbody").append(tRow)
+				}
+			});
+
+		})
+	
 	});	
-// });
+
+
+//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=cuzzy&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyCoqQeit3I0Arb3_C-BdKSsFYX_yokU4f4
 
